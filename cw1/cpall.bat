0e009382d5794d73582c4e06f07f739ccd9dfc33
ShiBin Teo <62151367+teoshibin@users.noreply.github.com>
2021-11-29 01:03:36 +0000

    Update cpall.bat

--- cw1/cpall.bat
+++ cw1/cpall.bat
@@ -7,15 +7,15 @@ echo Done
 
 echo.
 echo Compiling testing programs...
-mtc testing\test0.mt --compile
-mtc testing\test1.mt --compile
-mtc testing\test2.mt --compile
-mtc testing\test3.mt --compile
-mtc testing\test4.mt --compile
-mtc testing\test5.mt --compile
-mtc testing\error1.mt --compile
-mtc testing\error2.mt --compile
-mtc testing\error3.mt --compile
+mtc testing\test0.mt 
+mtc testing\test1.mt 
+mtc testing\test2.mt 
+mtc testing\test3.mt 
+mtc testing\test4.mt 
+mtc testing\test5.mt 
+mtc testing\error1.mt
+mtc testing\error2.mt
+mtc testing\error3.mt
 echo Done
 echo.
  
